70ca817952e6853973c63b0de3a5a89f5063433d
Kirill <kirillstengauer@gmail.com>
2024-10-30 15:22:22 +0400

    ФИО: Штенгауэр Кирилл Алексеевич Номер группы: 6211 Номер лабораторной: 5 Номер варианта: 07 Краткое описание решаемой задачи: Было реализованно приложение с графическим интерфейсом, в котором присутствует возможность выбора нужного csv файла. После того как файл был выбран изображения из выбранной аннотации начинают транслироваться на экране. С помощью итератора из 2 лабораторной работы были реализованы кнопки, позволяющие перемещаться по изображениям, включенный в файл аннотации.

---
+++ main_window.py
@@ -0,0 +1,144 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from laba2 import ImageIterator
+
+
+class Ui_MainWindow(object):
+    def __init__(self):
+        self.current_index = 0
+        self.data = None
+        self.file_name = None
+        self.img = []
+
+
+    def setupUi(self, MainWindow: QtWidgets.QMainWindow)->None:
+        """
+        Sets up the main UI components of the MainWindow
+        :param MainWindow: The main window instance where UI elements are added
+        """
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1600, 900)
+        MainWindow.setStyleSheet("background-color: rgb(170, 85, 127)")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.button_back = QtWidgets.QPushButton(self.centralwidget)
+        self.button_back.setGeometry(QtCore.QRect(90, 700, 170, 70))
+        self.button_back.setStyleSheet("background-color: rgb(114, 147, 255)")
+        self.button_back.setObjectName("back")
+        self.button_back.clicked.connect(self.show_previous_item)
+
+        self.button_forward = QtWidgets.QPushButton(self.centralwidget)
+        self.button_forward.setGeometry(QtCore.QRect(260, 700, 170, 70))
+        self.button_forward.setStyleSheet("background-color: rgb(114, 147, 255)")
+        self.button_forward.setObjectName("forward")
+        self.button_forward.clicked.connect(self.show_next_item)
+
+        self.select_file = QtWidgets.QPushButton(self.centralwidget)
+        self.select_file.setGeometry(QtCore.QRect(1340, 700, 170, 70))
+        self.select_file.setStyleSheet("background-color: rgb(114, 147, 255)")
+        self.select_file.setObjectName("select_file")
+        self.select_file.clicked.connect(self.open_file_dialog)
+
+        self.photo = QtWidgets.QLabel(self.centralwidget)
+        self.photo.setGeometry(QtCore.QRect(10, 10, 1580, 680))
+        self.photo.setText("")
+        self.photo.setPixmap(QtGui.QPixmap("istockphoto-1442799465-640x640.jpg"))
+        self.photo.setScaledContents(True)
+        self.photo.setObjectName("photo")
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+    def retranslateUi(self, MainWindow: QtWidgets.QMainWindow)->None:
+        """
+        Sets the text for UI elements, allowing for translations.
+        :param MainWindow: The main window instance where UI elements are added
+        """
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+        self.button_back.setText(_translate("MainWindow", "back"))
+        self.button_forward.setText(_translate("MainWindow","forward"))
+        self.select_file.setText(_translate("MainWindow", "Select file"))
+
+
+    def get_images(self)->list:
+        """
+        Gets paths to images
+        :return: list of relative paths to images from csv file
+        """
+        self.data = ImageIterator.Iterator(self.file_name)
+        img = []
+        for row in self.data:
+            img.append(row[1])
+        return img
+
+
+    def open_file_dialog(self)->None:
+        """
+        Allows to select specific csv file by pressing clicking the desired button
+        """
+        options = QtWidgets.QFileDialog.Options()
+        self.file_name, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Выберите изображение", "",
+                                                             "CSV Files (*.csv)", options=options)
+        if not self.file_name: return
+
+        self.img = self.get_images()
+        self.load_image(0)
+
+
+    def load_image(self, count)->None:
+        """
+        Displays the image on the screen
+        :param count: the index of current image
+        """
+        if len(self.img) == 0:
+            print("Список изображений пуст.")
+            return
+        image_path=self.img[count]
+        self.photo.setPixmap(QtGui.QPixmap(image_path))
+
+
+    def show_next_item(self)->None:
+        """
+        Allows to display the next image from csv file by pressing desired button
+        """
+        if len(self.img) == 0:
+            print("Список изображений пуст.")
+            return
+        if self.current_index == len(self.img) - 1:
+            self.current_index = 0
+        else:
+            self.current_index +=1
+        self.load_image(self.current_index)
+
+
+    def show_previous_item(self)->None:
+        """
+        Allows to display the previous image from csv file by pressing desired button
+        """
+        if len(self.img) == 0:
+            print("Список изображений пуст.")
+            return
+        if self.current_index <=0:
+            self.current_index = len(self.img) - 1
+        else:
+            self.current_index-=1
+        self.load_image(self.current_index)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
